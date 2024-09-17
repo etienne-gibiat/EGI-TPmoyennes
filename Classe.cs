eb663c84c9dd4944d4d5a7e441fe4615982a7c41
Formation <Formation@HNI-DN-VM2023>
2024-09-17 15:36:41 +0200

    Ajoutez des fichiers projet.

---
+++ Classe.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    internal class Classe
+    {
+            
+        public List<Eleve> eleves;
+        public List<String> matieres;
+        public readonly int maxEleves = 30;
+        public readonly int maxMatieres = 10;
+        public string nomClasse;
+
+        
+
+        public Classe(string nomClasse)
+        {
+            this.nomClasse = nomClasse;
+            this.eleves = new List<Eleve>();   
+            this.matieres = new List<String>(); 
+        }
+
+        internal void ajouterEleve(string nom, string Prenom)
+        {
+            if (maxEleves <= eleves.Count)
+                throw new IndexOutOfRangeException($"The collection eleves can hold only {maxEleves} elements.");
+            eleves.Add(new Eleve(nom, Prenom));
+            
+        }
+
+        internal void ajouterMatiere(string nomMatiere)
+        {
+            if (maxMatieres <= matieres.Count)
+                throw new IndexOutOfRangeException($"The collection matieres can hold only {maxMatieres} elements.");
+            matieres.Add(nomMatiere);
+
+        }
+
+        internal float moyenneGeneral()
+        {
+            float cumulMoyenne = 0;
+            for (int i = 0; i < matieres.Count; i++){
+                cumulMoyenne += moyenneMatiere(i);
+            }
+            return (float)(Math.Truncate(100 * (cumulMoyenne / matieres.Count)) / 100);
+        }
+
+        internal float moyenneMatiere(int nMatiere)
+        {
+            float cumulMoyenne = 0;
+            for(int i = 0; i < eleves.Count; i++)
+            {
+                cumulMoyenne += eleves[i].moyenneMatiere(nMatiere);
+            }
+            return (float)(Math.Truncate(100 * (cumulMoyenne / eleves.Count)) / 100);
+        }
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    internal class Eleve
+    {
+        public string nom;
+        public string prenom;
+        public List<Note> notes;
+        public readonly int maxNotes = 200;
+        private int nextNoteIndex = 0;
+
+        public Eleve(string nom, string prenom)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.notes = new List<Note>();
+        }
+
+        internal void ajouterNote(Note note)
+        {
+
+            if (maxNotes <= notes.Count)
+                throw new IndexOutOfRangeException($"The collection notes can hold only {maxNotes} elements.");
+            notes.Add(note);
+            
+        }
+
+        internal float moyenneGeneral()
+        {
+            float cumulNotes = 0;
+            int compteurNotes = 0;
+            float moyenneMatiere;
+            for (int i = 0; i < 10; i++) {
+                moyenneMatiere = this.moyenneMatiere(i);
+                if(moyenneMatiere != -1)
+                {
+                    cumulNotes += moyenneMatiere;
+                    compteurNotes++;
+                }         
+            }
+            return (float)(Math.Truncate(100 * (cumulNotes / compteurNotes)) / 100);
+        }
+
+        internal float moyenneMatiere(int nMatiere)
+        {
+            float cumulNotes = 0;
+            int compteurNotes = 0;
+            for(int i = 0;i < notes.Count; i++){
+                if (notes[i].matiere == nMatiere)
+                {
+                    cumulNotes += notes[i].note;
+                    compteurNotes++;
+                }
+                    
+            }
+            if(compteurNotes == 0)
+                return -1;
+            else
+                return (float)(Math.Truncate(100 * (cumulNotes / compteurNotes)) / 100);
+        }
+    }
+}
